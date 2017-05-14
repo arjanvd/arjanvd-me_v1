63821d0b955dc60b3b244d6ae314564e18c258dd
Arjan van Drielen <arjanvandrielen@gmail.com>
2017-05-14 21:24:23 +0200

    last commit

---
+++ backup 160920/textpattern/config.php
@@ -0,0 +1,9 @@
+<?php
+$txpcfg['db'] = '75937arjanvd';
+$txpcfg['user'] = '75937arjanvd';
+$txpcfg['pass'] = 'dixlixko';
+$txpcfg['host'] = 'sql2.pcextreme.nl';
+$txpcfg['table_prefix'] = '';
+$txpcfg['txpath'] = '/home/vhosting/c/vhost0075937/domains/arjanvd.nl/htdocs/www/textpattern';
+$txpcfg['dbcharset'] = 'utf8mb4';
+?>
